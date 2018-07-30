a4bd77fd4bc42fd719b6138f459e1508fe2a73c4
Md. Rana Hamid <1015052066.mrh@grad.cse.buet.ac.bd>
2018-07-31 00:58:34 +0600

    superior

---
+++ Shikhai/Controllers/TutorsController.cs
@@ -0,0 +1,24 @@
+﻿using Shikhai.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shikhai.Controllers
+{
+    public class TutorsController : Controller
+    {
+        // GET: Tutors
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HomePageSearch entity)
+        {
+            return View();
+        }
+
+    }
+}
